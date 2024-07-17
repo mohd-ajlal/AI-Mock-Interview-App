0ccafcbdc7fea5fb0bda26ec0ae8f374db0ce630
Mohd Ajlal <ajlal00786@gmail.com>
2024-07-18 01:11:04 +0530

    text edit

--- components/ui/google-gemini-effect.tsx
+++ components/ui/google-gemini-effect.tsx
@@ -30,6 +30,9 @@ export const GoogleGeminiEffect = ({
   return (
     <div className={cn("sticky top-80", className)}>
       <p className="text-lg md:text-7xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
+        {title || `AI MOCK INTERVIEW APP`}
+      </p>
+      <p className="text-lg md:text-5xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
         {title || `Build By Mohd Ajlal`}
       </p>
       <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
